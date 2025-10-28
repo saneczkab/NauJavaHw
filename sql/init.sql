926e6286b9dd6b5cc3d4716b2fb72d288ddeff42
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-10-28 21:36:55 +0500

    Update sql scripts

--- sql/init.sql
+++ sql/init.sql
@@ -56,6 +56,7 @@ ALTER TABLE UserTags ADD FOREIGN KEY (user_id) REFERENCES Users (id);
 ALTER TABLE UserTags ADD FOREIGN KEY (tag_id) REFERENCES Tags (id);
 
 
+-- Test data
 -- Insert values
 
 insert into Contents (name, description, used_symbols) values
@@ -71,3 +72,21 @@ insert into Algorithms (name, key_size, mode) values
 ('AES', 128, 'ECB'),
 ('DES', 56, 'ECB'),
 ('DESede', 168, 'ECB');
+
+insert into Tags (name, description, color_hex) values
+('Study', 'Study services passwords', '#FF0000'),
+('Social', 'Social media passwords', '#00FF00'),
+('Games', 'Game services passwords', '#0000FF');
+
+insert into Users (username, email, password_hash) values
+('alex', 'alex@mail.com', 'alex_hash'),
+('white_cat', 'white@cat.com', 'white_cat_hash'),
+('water_fox', 'water@fox.com', 'water_fox_hash');
+
+insert into Passwords (user_id, encrypted_password, content_id, algorithm_id, salt, length, updated_at) values
+(1, convert_to('123456789012', 'UTF8'), 1, 1, 'salt0', 12, now()),
+(1, convert_to('qwertyuiop111', 'UTF8'), 4, 2, 'salt1', 13, now()),
+(2, convert_to('1234567890123@', 'UTF8'), 7, 3, 'salt2', 14, now()),
+(3, convert_to('@43210987654321', 'UTF8'), 7, 1, 'salt3', 15, now());
+
+-- TODO: add test data for UserTags table, add PasswordTags table
