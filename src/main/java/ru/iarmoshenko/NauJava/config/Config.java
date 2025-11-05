d69ee4361cd91071caa2a7db446913f3534a6611
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-11-05 18:11:19 +0500

    Implement securityFilterChain (task 6)

--- src/main/java/ru/iarmoshenko/NauJava/config/Config.java
+++ src/main/java/ru/iarmoshenko/NauJava/config/Config.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.iarmoshenko.NauJava.entity.LegacyPassword;
 
 @Configuration
@@ -39,5 +42,16 @@ public class Config {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/registration", "/login", "/logout").permitAll()
+                        .requestMatchers( "/swagger-ui/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
     }
 }
