02295e6b6d51441cfea32f2f1decd2615c5f52d3
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-11-05 17:43:36 +0500

    Update UserService (task 1)

--- src/main/java/ru/iarmoshenko/NauJava/service/UserService.java
+++ src/main/java/ru/iarmoshenko/NauJava/service/UserService.java
@@ -1,5 +1,11 @@
 package ru.iarmoshenko.NauJava.service;
 
+import ru.iarmoshenko.NauJava.entity.User;
+
+import java.util.Optional;
+
 public interface UserService {
     void deleteUserById(Integer id);
+    User getUserByUsername(String username);
+    void createUser(String username, String email, String password);
 }
--- src/main/java/ru/iarmoshenko/NauJava/service/UserServiceImpl.java
+++ src/main/java/ru/iarmoshenko/NauJava/service/UserServiceImpl.java
@@ -4,8 +4,11 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import ru.iarmoshenko.NauJava.entity.User;
 import ru.iarmoshenko.NauJava.repository.PasswordRepository;
 import ru.iarmoshenko.NauJava.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,6 +31,37 @@ public class UserServiceImpl implements UserService {
             var passwords = passwordRepository.findByUserId(id);
             passwordRepository.deleteAll(passwords);
             userRepository.deleteById(id);
+            transactionManager.commit(status);
+        }
+        catch (DataAccessException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        var status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            var user = userRepository.findByUsernameOrEmail(username, null)
+                    .stream().findFirst().orElse(null);
+            transactionManager.commit(status);
+            return user;
+        }
+        catch (DataAccessException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+
+    @Override
+    public void createUser(String username, String email, String password) {
+        var status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            var user = new User(username, email, password);
+            userRepository.save(user);
             transactionManager.commit(status);
         }
         catch (DataAccessException e) {
