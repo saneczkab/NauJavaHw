e206244618b3aadae756c501df670112ee4e8a4e
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-11-05 18:08:36 +0500

    Add EnableWebSecurity annotation to Config (task 4)

--- src/main/java/ru/iarmoshenko/NauJava/config/Config.java
+++ src/main/java/ru/iarmoshenko/NauJava/config/Config.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import ru.iarmoshenko.NauJava.entity.LegacyPassword;
 
 @Configuration
+@EnableWebSecurity
 public class Config {
     @Value("${spring.application.name}")
     private String appName;
