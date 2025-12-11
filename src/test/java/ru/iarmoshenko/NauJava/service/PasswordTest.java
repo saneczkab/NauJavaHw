c702a93e6eecd6c53ad4418cca456272a75816d2
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-12-11 20:49:11 +0500

    Prepare test for updated passwordService.generatePasswords

--- src/test/java/ru/iarmoshenko/NauJava/service/PasswordTest.java
+++ src/test/java/ru/iarmoshenko/NauJava/service/PasswordTest.java
@@ -4,15 +4,43 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.iarmoshenko.NauJava.PasswordGeneratorTest;
+import ru.iarmoshenko.NauJava.entity.ContentType;
 import ru.iarmoshenko.NauJava.entity.Password;
 
 import java.util.List;
+import java.util.Random;
 
 /**
- * Тесты для проверки функционала репозитория паролей.
+ * Тесты для проверки функционала репозитория и сервиса паролей.
  */
 @SpringBootTest
 public class PasswordTest extends PasswordGeneratorTest {
+    /**
+     * Тест генерации паролей с проверкой корректности сохранения их в базе.
+     */
+    @Test
+    public void testGeneratePasswords() {
+        var random = new Random();
+        var user = users.getLast();
+        var length = random.nextInt(1, 64);
+        var count = random.nextInt(32, 64);
+        var content = ContentType.MIX;
+        List<String> generatedPasswords = passwordService.generatePassword(count, length, content, user.getId());
+        var userDecryptedPasswords = passwordRepository.findByUserId(user.getId())
+                .stream()
+                .map(pass -> passwordService.decryptPassword(pass.getEncryptedPassword(), pass.getSalt()))
+                .toList();
+
+        Assertions.assertNotNull(generatedPasswords);
+        Assertions.assertEquals(count, generatedPasswords.size());
+        for (var pass : generatedPasswords) {
+            Assertions.assertEquals(length, pass.length());
+            Assertions.assertTrue(userDecryptedPasswords.contains(pass));
+        }
+
+        Assertions.assertTrue(userDecryptedPasswords.contains(generatedPasswords));
+    }
+
     @Test
     public void testFindByUserId() {
         var firstUser = users.get(0);
